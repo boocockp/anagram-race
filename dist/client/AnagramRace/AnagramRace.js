b6d8b32fb6660efcc748f122874dde7cb3f6279e
boocockp <>
2024-08-05 10:13:57 +0100

    Full word list, Show A Letter

--- dist/client/AnagramRace/AnagramRace.js
+++ dist/client/AnagramRace/AnagramRace.js
@@ -2,21 +2,22 @@ const runtimeUrl = window.elementoRuntimeUrl || 'https://elemento.online/lib/run
 const Elemento = await import(runtimeUrl)
 const {React, trace, elProps, stateProps, wrapFn} = Elemento
 const {importModule, importHandlers} = Elemento
-const WordList = await import('../files/words1.js').then(...importHandlers('Words'))
+const WordList = await import('../files/words2.js').then(...importHandlers('Words'))
 
 // MainPage.js
 function MainPage(props) {
     const pathTo = name => props.path + '.' + name
     const {Page, TextElement, Timer, Data, Calculation, Dialog, Button, Block, TextInput, Icon} = Elemento.components
-    const {Floor, Len, And, Not, Or, RandomFrom, Join, Shuffle, Split, Ceiling, If, Eq} = Elemento.globalFunctions
+    const {Floor, Len, And, Not, Or, RandomFrom, Join, Shuffle, Split, Ceiling, Left, If, Eq, Lowercase, Trim, Lte} = Elemento.globalFunctions
     const {Reset, Set} = Elemento.appFunctions
     const _state = Elemento.useGetStore()
     const Status = _state.setObject(pathTo('Status'), new Data.State(stateProps(pathTo('Status')).value('Ready').props))
     const Score = _state.setObject(pathTo('Score'), new Data.State(stateProps(pathTo('Score')).value(0).props))
     const TheWord = _state.setObject(pathTo('TheWord'), new Data.State(stateProps(pathTo('TheWord')).props))
+    const LettersShown = _state.setObject(pathTo('LettersShown'), new Data.State(stateProps(pathTo('LettersShown')).value(0).props))
     const Points = React.useCallback(wrapFn(pathTo('Points'), 'calculation', (word) => {
-        return Floor(Len(word) * 1.5)
-    }), [])
+        return Floor(Len(word) * 2 - LettersShown * 3)
+    }), [LettersShown])
     const ScrambledWord = _state.setObject(pathTo('ScrambledWord'), new Data.State(stateProps(pathTo('ScrambledWord')).props))
     const GivenUp = _state.setObject(pathTo('GivenUp'), new Data.State(stateProps(pathTo('GivenUp')).value(false).props))
     const GameRunning = _state.setObject(pathTo('GameRunning'), new Calculation.State(stateProps(pathTo('GameRunning')).value(Or(Status == 'Playing', Status == 'Paused')).props))
@@ -37,16 +38,18 @@ function MainPage(props) {
     const PausePanel = _state.setObject(pathTo('PausePanel'), new Block.State(stateProps(pathTo('PausePanel')).props))
     const PlayPanel = _state.setObject(pathTo('PlayPanel'), new Block.State(stateProps(pathTo('PlayPanel')).props))
     const QuestionLayout = _state.setObject(pathTo('QuestionLayout'), new Block.State(stateProps(pathTo('QuestionLayout')).props))
+    const CluesLayout = _state.setObject(pathTo('CluesLayout'), new Block.State(stateProps(pathTo('CluesLayout')).props))
     const AnswerLayout = _state.setObject(pathTo('AnswerLayout'), new Block.State(stateProps(pathTo('AnswerLayout')).props))
     const Attempt = _state.setObject(pathTo('Attempt'), new TextInput.State(stateProps(pathTo('Attempt')).value(If(GivenUp, TheWord, '')).props))
     const StartNewWord = React.useCallback(wrapFn(pathTo('StartNewWord'), 'calculation', () => {
         let word = RandomFrom(WordList())
         Set(TheWord, word)
         Set(ScrambledWord, Join(Shuffle(Split(word))))
+        Reset(LettersShown)
         Reset(Attempt)
         Reset(GivenUp)
         return Attempt.Focus()
-    }), [TheWord, ScrambledWord, Attempt, GivenUp])
+    }), [TheWord, ScrambledWord, LettersShown, Attempt, GivenUp])
     const StartNewGame = React.useCallback(wrapFn(pathTo('StartNewGame'), 'calculation', () => {
         Reset(Attempt)
         Reset(GivenUp)
@@ -66,8 +69,9 @@ function MainPage(props) {
     const IsCorrect_whenTrueAction = React.useCallback(wrapFn(pathTo('IsCorrect'), 'whenTrueAction', async () => {
         Set(Score, Score + (await Points(TheWord)))
     }), [Score, Points, TheWord])
-    const IsCorrect = _state.setObject(pathTo('IsCorrect'), new Calculation.State(stateProps(pathTo('IsCorrect')).value(And(Eq(Attempt, TheWord), Not(GivenUp))).whenTrueAction(IsCorrect_whenTrueAction).props))
+    const IsCorrect = _state.setObject(pathTo('IsCorrect'), new Calculation.State(stateProps(pathTo('IsCorrect')).value(And(Eq(Lowercase(Trim(Attempt)), TheWord), Not(GivenUp))).whenTrueAction(IsCorrect_whenTrueAction).props))
     const Answering = _state.setObject(pathTo('Answering'), new Calculation.State(stateProps(pathTo('Answering')).value(And(TheWord, Not(IsCorrect), Not(GivenUp))).props))
+    const WordButtons = _state.setObject(pathTo('WordButtons'), new Block.State(stateProps(pathTo('WordButtons')).props))
     const EndedPanel = _state.setObject(pathTo('EndedPanel'), new Block.State(stateProps(pathTo('EndedPanel')).props))
     const ControlsLayout = _state.setObject(pathTo('ControlsLayout'), new Block.State(stateProps(pathTo('ControlsLayout')).props))
     const StartGame2_action = React.useCallback(wrapFn(pathTo('StartGame2'), 'action', async () => {
@@ -80,6 +84,10 @@ function MainPage(props) {
     const GiveUp_action = React.useCallback(wrapFn(pathTo('GiveUp'), 'action', () => {
         Set(GivenUp, true)
     }), [GivenUp])
+    const ShowALetter_action = React.useCallback(wrapFn(pathTo('ShowALetter'), 'action', async () => {
+        Set(LettersShown, LettersShown + 1)
+        await Attempt.Focus()
+    }), [LettersShown, Attempt])
     const StartGame_action = React.useCallback(wrapFn(pathTo('StartGame'), 'action', async () => {
         await StartNewGame()
     }), [StartNewGame])
@@ -103,6 +111,7 @@ function MainPage(props) {
         React.createElement(Data, elProps(pathTo('Status')).display(false).props),
         React.createElement(Data, elProps(pathTo('Score')).display(false).props),
         React.createElement(Data, elProps(pathTo('TheWord')).display(false).props),
+        React.createElement(Data, elProps(pathTo('LettersShown')).display(false).props),
         React.createElement(Data, elProps(pathTo('ScrambledWord')).display(false).props),
         React.createElement(Data, elProps(pathTo('GivenUp')).display(false).props),
         React.createElement(Calculation, elProps(pathTo('Answering')).show(false).props),
@@ -117,18 +126,22 @@ For each word you will see the scrambled version and the points you can earn.  T
 Click Skip Word to give up and show the answer.
 
 
+Click Show A Letter to show some of the initial letters - but you will score fewer points.
+
+
 Click Next Word to move on to the next word.
 
 
 <b>Tips</b>
-
-- Words may be plural
-- You can Pause the game, but it will skip the word you're on`).props),
+<ul>
+<li>Words may be plural</li>
+<li>You can Pause the game, but it will skip the word you're on</li>
+</b>`).props),
             React.createElement(Button, elProps(pathTo('StartGame2')).content('Start Game').appearance('filled').show(Not(GameRunning)).action(StartGame2_action).props),
     ),
-        React.createElement(Block, elProps(pathTo('StatsLayout')).layout('horizontal').styles(elProps(pathTo('StatsLayout.Styles')).fontSize('32').props).props,
+        React.createElement(Block, elProps(pathTo('StatsLayout')).layout('horizontal').styles(elProps(pathTo('StatsLayout.Styles')).fontSize('24').props).props,
             React.createElement(TextElement, elProps(pathTo('ScoreDisplay')).show(Or(GameRunning, Status == 'Ended')).styles(elProps(pathTo('ScoreDisplay.Styles')).fontSize('inherit').color('blue').marginRight('100').props).content(Score + ' points').props),
-            React.createElement(TextElement, elProps(pathTo('TimeDisplay')).show(GameRunning).styles(elProps(pathTo('TimeDisplay.Styles')).fontSize('inherit').color('green').props).content(Ceiling(GameTimer. remainingTime) + ' seconds left').props),
+            React.createElement(TextElement, elProps(pathTo('TimeDisplay')).show(GameRunning).styles(elProps(pathTo('TimeDisplay.Styles')).fontSize('inherit').color('green').props).content(Ceiling(GameTimer. remainingTime) + 's left').props),
             React.createElement(TextElement, elProps(pathTo('GameOver')).show(Status == 'Ended').styles(elProps(pathTo('GameOver.Styles')).fontSize('inherit').color('white').backgroundColor('green').padding('0 0.5em').borderRadius('8px').props).content('Game Over').props),
     ),
         React.createElement(Block, elProps(pathTo('ReadyPanel')).layout('vertical').show(Status == 'Ready').props,
@@ -148,14 +161,20 @@ Or Start Game to dive right in!`).props),
             React.createElement(TextElement, elProps(pathTo('ScrambledLetters')).styles(elProps(pathTo('ScrambledLetters.Styles')).fontSize('20').letterSpacing('1px').props).content(ScrambledWord).props),
             React.createElement(TextElement, elProps(pathTo('PointsToWin')).styles(elProps(pathTo('PointsToWin.Styles')).paddingTop('3').marginLeft('5em').props).content(Points(TheWord) + ' points').props),
     ),
-            React.createElement(Block, elProps(pathTo('AnswerLayout')).layout('horizontal wrapped').styles(elProps(pathTo('AnswerLayout.Styles')).marginTop('20px').props).props,
+            React.createElement(Block, elProps(pathTo('CluesLayout')).layout('horizontal').props,
+            React.createElement(TextElement, elProps(pathTo('StartsWith')).show(LettersShown > 0).styles(elProps(pathTo('StartsWith.Styles')).fontSize('20').letterSpacing('1px').props).content('Starts with: ' + Left(TheWord, LettersShown)).props),
+    ),
+            React.createElement(Block, elProps(pathTo('AnswerLayout')).layout('horizontal wrapped').styles(elProps(pathTo('AnswerLayout.Styles')).props).props,
             React.createElement(TextInput, elProps(pathTo('Attempt')).label('Your Answer').readOnly(Not(Answering)).styles(elProps(pathTo('Attempt.Styles')).fontSize('20').marginTop('20px').props).props),
             React.createElement(Icon, elProps(pathTo('CorrectIndicator')).iconName('check_circle').show(IsCorrect).styles(elProps(pathTo('CorrectIndicator.Styles')).fontSize('40').color('green').props).props),
             React.createElement(TextElement, elProps(pathTo('WordPoints')).show(IsCorrect).content(Points(TheWord) + ' points added').props),
     ),
             React.createElement(Calculation, elProps(pathTo('IsCorrect')).show(false).props),
+            React.createElement(Block, elProps(pathTo('WordButtons')).layout('horizontal wrapped').props,
             React.createElement(Button, elProps(pathTo('NextWord')).content('Next Word').appearance('outline').show(Not(Answering)).enabled(GameTimer.isRunning).action(NextWord_action).props),
             React.createElement(Button, elProps(pathTo('GiveUp')).content('Skip Word').appearance('outline').show(Answering).action(GiveUp_action).props),
+            React.createElement(Button, elProps(pathTo('ShowALetter')).content('Show A Letter').appearance('outline').show(Answering).enabled(Lte(LettersShown, 2)).action(ShowALetter_action).props),
+    ),
     ),
         React.createElement(Block, elProps(pathTo('EndedPanel')).layout('vertical').show(Status == 'Ended').props,
             React.createElement(TextElement, elProps(pathTo('TextA')).styles(elProps(pathTo('TextA.Styles')).fontFamily('Luckiest Guy').fontSize('28').color('#7529df').props).content('Congratulations!').props),
